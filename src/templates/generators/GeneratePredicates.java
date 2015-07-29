576f090f8fb389de3644380c4782c1a9a0b3261b
TeamworkGuy2 <straitfrommars@rocketmail.com>
2015-07-29 02:08:41 +0000

    Updated templates to use new template functions. Added PropertyAccessor and SimplePropertyAccessor that provide access to a getter/setter pair of functions.

--- src/templates/generators/GeneratePredicates.java
+++ src/templates/generators/GeneratePredicates.java
@@ -12,8 +12,8 @@ import java.util.List;
 import java.util.concurrent.ThreadPoolExecutor;
 
 import codeTemplate.ClassTemplateBuilder;
-import codeTemplate.PrimitiveClassTemplate;
-import codeTemplate.TemplateUtil;
+import codeTemplate.PrimitiveClassTemplateDeprecated;
+import codeTemplate.TemplateRender;
 
 /**
  * @author TeamworkGuy2
@@ -23,42 +23,42 @@ public class GeneratePredicates {
 
 
 	public static void generatePrimitivePredicates() {
-		TemplateUtil.renderTemplates("src/templates/TPredicate.stg", "TPredicate", getPrimitiveTemplateInfos("$Type$Predicate", true, true));
+		TemplateRender.renderClassTemplates("src/templates/TPredicate.stg", "TPredicate", getPrimitiveTemplateInfos("$Type$Predicate", true, true));
 	}
 
 
 	public static void generatePrimitiveConsumers() {
-		TemplateUtil.renderTemplates("src/templates/TConsumer.stg", "TConsumer", getPrimitiveTemplateInfos("$Type$Consumer", false, false));		
+		TemplateRender.renderClassTemplates("src/templates/TConsumer.stg", "TConsumer", getPrimitiveTemplateInfos("$Type$Consumer", false, false));		
 	}
 
 
 	public static void generatePrimitiveSuppliers() {
-		TemplateUtil.renderTemplates("src/templates/TSupplier.stg", "TSupplier", getPrimitiveTemplateInfos("$Type$Supplier", false, false));
+		TemplateRender.renderClassTemplates("src/templates/TSupplier.stg", "TSupplier", getPrimitiveTemplateInfos("$Type$Supplier", false, false));
 	}
 
 
-	public static List<PrimitiveClassTemplate> getPrimitiveTemplateInfos(String classNameTmpl, boolean includeString, boolean includeEnum) {
-		List<PrimitiveClassTemplate> tmpls = new ArrayList<>();
-		tmpls.add( inferPropertyNames(newByteTemplate(new PrimitiveClassTemplate(), classNameTmpl, "functionUtils")) );
-		tmpls.add( inferPropertyNames(newCharTemplate(new PrimitiveClassTemplate(), classNameTmpl, "functionUtils")) );
-		tmpls.add( inferPropertyNames(newShortTemplate(new PrimitiveClassTemplate(), classNameTmpl, "functionUtils")) );
-		tmpls.add( inferPropertyNames(newFloatTemplate(new PrimitiveClassTemplate(), classNameTmpl, "functionUtils")) );
+	public static List<PrimitiveClassTemplateDeprecated> getPrimitiveTemplateInfos(String classNameTmpl, boolean includeString, boolean includeEnum) {
+		List<PrimitiveClassTemplateDeprecated> tmpls = new ArrayList<>();
+		tmpls.add( inferPropertyNames(newByteTemplate(new PrimitiveClassTemplateDeprecated(), classNameTmpl, "functionUtils")) );
+		tmpls.add( inferPropertyNames(newCharTemplate(new PrimitiveClassTemplateDeprecated(), classNameTmpl, "functionUtils")) );
+		tmpls.add( inferPropertyNames(newShortTemplate(new PrimitiveClassTemplateDeprecated(), classNameTmpl, "functionUtils")) );
+		tmpls.add( inferPropertyNames(newFloatTemplate(new PrimitiveClassTemplateDeprecated(), classNameTmpl, "functionUtils")) );
 
 		if(includeString) {
-			ClassTemplateBuilder<PrimitiveClassTemplate> stringInfo = new ClassTemplateBuilder<>(new PrimitiveClassTemplate(), classNameTmpl, "functionUtils");
+			ClassTemplateBuilder<PrimitiveClassTemplateDeprecated> stringInfo = new ClassTemplateBuilder<>(new PrimitiveClassTemplateDeprecated(), classNameTmpl, "functionUtils");
 			stringInfo.getTemplate().type = "String";
 			stringInfo.getTemplate().typeShort = "String";
-			stringInfo.getTemplate().typeShortUpperCase = "String";
+			stringInfo.getTemplate().typeShortTitleCase = "String";
 			stringInfo.addImportStatement(ThreadPoolExecutor.class);
 			tmpls.add( inferPropertyNames(stringInfo.getTemplate()) );
 		}
 
 		if(includeEnum) {
-			ClassTemplateBuilder<PrimitiveClassTemplate> enumInfo = new ClassTemplateBuilder<>(new PrimitiveClassTemplate(), classNameTmpl, "functionUtils");
+			ClassTemplateBuilder<PrimitiveClassTemplateDeprecated> enumInfo = new ClassTemplateBuilder<>(new PrimitiveClassTemplateDeprecated(), classNameTmpl, "functionUtils");
 			enumInfo.addTypeParameter("T", "T extends Enum<T>");
 			enumInfo.getTemplate().type = "T";
 			enumInfo.getTemplate().typeShort = "Enum";
-			enumInfo.getTemplate().typeShortUpperCase = "Enum";
+			enumInfo.getTemplate().typeShortTitleCase = "Enum";
 			tmpls.add( inferPropertyNames(enumInfo.getTemplate()) );
 		}
 
